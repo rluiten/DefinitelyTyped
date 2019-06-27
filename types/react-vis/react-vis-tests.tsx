0305bd55f7ede6fd80ead3b5a2bc490049d4c5ce
Tyler Miller <millety@americas.corpdir.net>
2019-06-27 14:34:27 -0700

    Added support for Uber's react-vis

---
+++ types/react-vis/react-vis-tests.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { HorizontalGridLines, XYPlot, XAxis, YAxis, LineSeries } from 'react-vis';
+
+class CustomLineSeries extends React.Component {
+    render() {
+        return (
+            <XYPlot width={300} height={300}>
+                <HorizontalGridLines />
+                <LineSeries data={[{ x: 1, y: 10 }, { x: 2, y: 5 }, { x: 3, y: 15 }]} />
+                <XAxis />
+                <YAxis />
+            </XYPlot>
+        );
+    }
+}
+
